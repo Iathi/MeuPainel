c326b3627f04a33cde1f55cdd937a2aabcc41f6d
Iathi <98141620+Iathi@users.noreply.github.com>
2024-10-11 12:38:06 -0300

    Update app.py

--- app.py
+++ app.py
@@ -119,4 +119,4 @@ def stop_sending():
     return jsonify(session.get('status', {'sending': [], 'errors': []}))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
